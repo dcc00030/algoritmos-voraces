97e07e5791dbe4b9d03bff47d3e2f8c70b79186c
dcasquel <dcasquel@Daniel>
2016-04-09 17:46:30 +0200

    ejercicio 8 voraces

--- main.cpp
+++ main.cpp
@@ -18,8 +18,28 @@ using namespace std;
 /*
  * 
  */
+void seleccion(int horario[][4], int charlas[],int tam){
+    if(tam == 0){
+        horario[1][0]=charlas[0];
+    }else{
+        
+    }
+}
+void organizar(int horario[][4], int charlas[]){
+    int tam = 0;
+    while(tam < 8){
+        seleccion(horario,charlas,tam);
+        tam++;
+    }
+}
 int main(int argc, char** argv) {
-
+    int horario[4][4]={
+        0,1,2,3,
+        -1,-1,-1,-1,
+        -1,-1,-1,-1,
+        -1,-1,-1,-1};//en la primera fila insertaremos las distintas aulas en el resto haremos como un horario
+    int charlas[8]={3,8,9,2,3,3,9,8};
+    organizar(horario,charlas);
     return 0;
 }
 
